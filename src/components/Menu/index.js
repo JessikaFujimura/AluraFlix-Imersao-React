03646d642991a497e68fdf58758ef9dc7cb7192b
JessikaFujimura <jessikafujimura@gmail.com>
2020-07-28 11:54:00 -0300

    Adicionando pages de cadastro

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Logo from '../../assets/img/Logo.png'
 import './styles.css'
 import Button from '../Button'
+import {Link} from 'react-router-dom'
 
 function Menu () {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
             <img className="Logo" src={Logo} alt="Logo"/>
-            </a>
-            <Button as="a" href="/"  className="ButtonLink">
+            </Link>
+
+            <Button as={Link} to="/cadastro/video"  className="ButtonLink">
                 Novo video
             </Button>
         </nav>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import Menu from './components/Menu'
-import Footer from './components/Footer'
-import BannerMain from './components/BannerMain'
-import Carousel from './components/Carousel'
-import Data from './data/dados_iniciais.json'
+import PageDefault from '../../components/PageDefault'
+import BannerMain from '../../components/BannerMain'
+import Carousel from '../../components/Carousel'
+import Data from '../../data/dados_iniciais.json'
 
-function App() {
+function Home() {
   return (
-    <>
-      <Menu />
-
+    <PageDefault>
       <BannerMain 
         videoTitle="O que faz uma desenvolvedora front-end? #HipstersPontoTube"
         videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
         url="https://www.youtube.com/watch?v=ZY3-MFxVdEw&feature=emb_title"
       />
-
       {Data.categorias.map(i => <Carousel 
       ignoreFirstVideo="true"
       category = {i}
       />)}
-
-      <Footer />
-    </>
+    </PageDefault>
   );
 }
 
-export default App;
+export default Home;
